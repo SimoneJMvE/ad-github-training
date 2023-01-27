73dc6238268265d5b9a6815bb4cf1f620abe572e
Simone van Eekert <pnl0qr8t@Emea.Royalahold.net>
2023-01-27 11:51:03 +0100

    new commit function

--- exercises/exercise_1/opdracht_1.py
+++ exercises/exercise_1/opdracht_1.py
@@ -1,2 +1,14 @@
 def likes(team: list) -> str:
-    return True
+    if len(team) == 0:
+        return "no one likes this"
+    elif len(team) == 1:
+        return f"{team[0]}likes this"
+    elif len(team)  == 2:
+        return f"{team[0]} en {team[1]} likes this"
+
+    else:
+        return  "To many people like your post"
+
+
+
+
